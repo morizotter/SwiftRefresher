f95406289f398552bf2df236bd86c44afc21d1b5
Yasuaki Goto <yasu1003@gmail.com>
2016-12-02 19:14:41 +0900

    [update] convert to swift3

--- SwiftRefresher/Refresher.swift
+++ SwiftRefresher/Refresher.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 public extension UIScrollView {
-    public func srf_addRefresher(refresher: Refresher) {
-        insertSubview(refresher, atIndex: 0)
+    public func srf_addRefresher(_ refresher: Refresher) {
+        insertSubview(refresher, at: 0)
         refresher.setup()
     }
     
     public func srf_removeRefresher() {
-        guard let refreshers = srf_findRefreshers() where refreshers.count > 0 else { return }
+        guard let refreshers = srf_findRefreshers(), refreshers.count > 0 else { return }
         refreshers.forEach {
             $0.removeFromSuperview()
         }
@@ -27,50 +27,50 @@ public extension UIScrollView {
         }
     }
     
-    private func srf_findRefreshers() -> [Refresher]? {
+    fileprivate func srf_findRefreshers() -> [Refresher]? {
         return subviews.filter { $0 is Refresher }.flatMap { $0 as? Refresher }
     }
 }
 
 public protocol SwfitRefresherEventReceivable {
-    func didReceiveEvent(event: SwiftRefresherEvent)
+    func didReceiveEvent(_ event: SwiftRefresherEvent)
 }
 
 public enum SwiftRefresherState {
-    case None
-    case Pulling
-    case Refreshing
-    case RecoveringInitialState
+    case none
+    case pulling
+    case refreshing
+    case recoveringInitialState
 }
 
 public enum SwiftRefresherEvent {
-    case Pull(offset: CGPoint, threshold: CGFloat)
-    case StartRefreshing
-    case EndRefreshing
-    case RecoveredToInitialState
+    case pull(offset: CGPoint, threshold: CGFloat)
+    case startRefreshing
+    case endRefreshing
+    case recoveredToInitialState
 }
 
 public typealias SwiftRefresherStartRefreshingHandler = (() -> Void)
-public typealias SwiftRefresherEventHandler = ((event: SwiftRefresherEvent) -> Void)
+public typealias SwiftRefresherEventHandler = ((_ event: SwiftRefresherEvent) -> Void)
 public typealias SwiftRefresherCustomRefreshViewCreator = (() -> SwfitRefresherEventReceivable)
 
 private let DEFAULT_HEIGHT: CGFloat = 44.0
 
-public class Refresher: UIView {
-    private var stateInternal = SwiftRefresherState.None
-    private var eventHandler: SwiftRefresherEventHandler?
-    private var startRefreshingHandler: SwiftRefresherStartRefreshingHandler?
-    private var contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
-    private var contentOffset = CGPoint.zero
-    private var distanceOffset: CGPoint {
+open class Refresher: UIView {
+    fileprivate var stateInternal = SwiftRefresherState.none
+    fileprivate var eventHandler: SwiftRefresherEventHandler?
+    fileprivate var startRefreshingHandler: SwiftRefresherStartRefreshingHandler?
+    fileprivate var contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+    fileprivate var contentOffset = CGPoint.zero
+    fileprivate var distanceOffset: CGPoint {
         return CGPoint(x: contentInset.left + contentOffset.x, y: contentInset.top + contentOffset.y)
     }
-    private var recoveringInitialState: Bool = false
-    private var refreshView: SwfitRefresherEventReceivable!
-    private var customRefreshViewCreator: SwiftRefresherCustomRefreshViewCreator?
+    fileprivate var recoveringInitialState: Bool = false
+    fileprivate var refreshView: SwfitRefresherEventReceivable!
+    fileprivate var customRefreshViewCreator: SwiftRefresherCustomRefreshViewCreator?
     
-    public var state: SwiftRefresherState { return stateInternal }
-    public var height: CGFloat = DEFAULT_HEIGHT
+    open var state: SwiftRefresherState { return stateInternal }
+    open var height: CGFloat = DEFAULT_HEIGHT
     
     deinit {
         if let scrollView = superview as? UIScrollView {
@@ -79,22 +79,22 @@ public class Refresher: UIView {
         }
     }
     
-    convenience public init(eventHandler: SwiftRefresherEventHandler) {
+    convenience public init(eventHandler: @escaping SwiftRefresherEventHandler) {
         self.init()
         self.eventHandler = eventHandler
     }
     
-    convenience public init(startRefreshingHandler: SwiftRefresherStartRefreshingHandler) {
+    convenience public init(startRefreshingHandler: @escaping SwiftRefresherStartRefreshingHandler) {
         self.init()
         self.startRefreshingHandler = startRefreshingHandler
     }
     
-    public override func willMoveToSuperview(newSuperview: UIView?) {
-        super.willMoveToSuperview(newSuperview)
+    open override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
         
         if let newSuperview = newSuperview {
             if let scrollView = newSuperview as? UIScrollView {
-                let options: NSKeyValueObservingOptions = [.Initial, .New]
+                let options: NSKeyValueObservingOptions = [.initial, .new]
                 scrollView.addObserver(self, forKeyPath: "contentOffset", options: options, context: nil)
                 scrollView.addObserver(self, forKeyPath: "contentInset", options: options, context: nil)
             }
@@ -106,7 +106,7 @@ public class Refresher: UIView {
         }
     }
     
-    override public func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+    override open func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         
         guard let scrollView = superview as? UIScrollView else { return }
         guard let keyPath = keyPath else { return }
@@ -114,117 +114,117 @@ public class Refresher: UIView {
         guard let _ = object else { return }
         
         if keyPath == "contentInset" {
-            if let value = change["new"] as? NSValue {
-                contentInset = value.UIEdgeInsetsValue()
+            if let value: NSValue = change[NSKeyValueChangeKey.newKey] as? NSValue {
+                contentInset = value.uiEdgeInsetsValue
             }
         }
         
         if keyPath == "contentOffset" {
-            if let value = change["new"] as? NSValue {
-                contentOffset = value.CGPointValue()
+            if let value: NSValue = change[NSKeyValueChangeKey.newKey] as? NSValue {
+                contentOffset = value.cgPointValue
             }
         }
         
         switch state {
-        case .None:
+        case .none:
             if distanceOffset.y <= 0 {
-                fireEvent(.Pull(offset: distanceOffset, threshold: -height))
+                fireEvent(.pull(offset: distanceOffset, threshold: -height))
             }
-        case .Pulling:
+        case .pulling:
             if distanceOffset.y >= 0 {
-                hidden = true
+                isHidden = true
             } else {
-                hidden = false
+                isHidden = false
             }
             
             if distanceOffset.y <= 0 {
-                fireEvent(.Pull(offset: distanceOffset, threshold: -height))
+                fireEvent(.pull(offset: distanceOffset, threshold: -height))
             }
             
-            if scrollView.decelerating && distanceOffset.y < -height {
+            if scrollView.isDecelerating && distanceOffset.y < -height {
                 startRefresh()
             }
-        case .RecoveringInitialState:
+        case .recoveringInitialState:
             break
-        case .Refreshing:
+        case .refreshing:
             break
         }
     }
     
-    public func setup() {
+    open func setup() {
         translatesAutoresizingMaskIntoConstraints = false
         let selfConstraints = [
-            NSLayoutConstraint(item: self, attribute: .Top, relatedBy: .Equal, toItem: superview, attribute: .Top, multiplier: 1.0, constant: -height),
-            NSLayoutConstraint(item: self, attribute: .Width, relatedBy: .Equal, toItem: superview, attribute: .Width, multiplier: 1.0, constant: 0.0)
+            NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: superview, attribute: .top, multiplier: 1.0, constant: -height),
+            NSLayoutConstraint(item: self, attribute: .width, relatedBy: .equal, toItem: superview, attribute: .width, multiplier: 1.0, constant: 0.0)
         ]
         superview?.addConstraints(selfConstraints)
         addConstraint(
-            NSLayoutConstraint(item: self, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .Height, multiplier: 1.0, constant: height)
+            NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: height)
         )
         
         clipsToBounds = true
         
-        refreshView = customRefreshViewCreator?() ?? SimpleRefreshView(activityIndicatorViewStyle: .Gray)
+        refreshView = customRefreshViewCreator?() ?? SimpleRefreshView(activityIndicatorViewStyle: .gray)
         guard let r = refreshView as? UIView else {
             fatalError("CustomRefreshView must be a subclass of UIView")
         }
         addSubview(r)
         r.translatesAutoresizingMaskIntoConstraints = false
         let constraints = [
-            NSLayoutConstraint(item: r, attribute: .Left, relatedBy: .Equal, toItem: self, attribute: .Left, multiplier: 1.0, constant: 0.0),
-            NSLayoutConstraint(item: r, attribute: .Top, relatedBy: .Equal, toItem: self, attribute: .Top, multiplier: 1.0, constant: 0.0),
-            NSLayoutConstraint(item: r, attribute: .Right, relatedBy: .Equal, toItem: self, attribute: .Right, multiplier: 1.0, constant: 0.0),
-            NSLayoutConstraint(item: r, attribute: .Bottom, relatedBy: .Equal, toItem: self, attribute: .Bottom, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: r, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: r, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: r, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: r, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0.0),
         ]
         addConstraints(constraints)
     }
     
-    public func createCustomRefreshView(creator: SwiftRefresherCustomRefreshViewCreator) {
+    open func createCustomRefreshView(_ creator: @escaping SwiftRefresherCustomRefreshViewCreator) {
         self.customRefreshViewCreator = creator
     }
     
-    private func startRefresh() {
+    fileprivate func startRefresh() {
         guard let scrollView = superview as? UIScrollView else { return }
-        if state == .Refreshing { return }
-        stateInternal = .Refreshing
-        UIView.animateWithDuration(0.25) { [weak self] () -> Void in
+        if state == .refreshing { return }
+        stateInternal = .refreshing
+        UIView.animate(withDuration: 0.25, animations: { [weak self] () -> Void in
             guard let s = self else { return }
             scrollView.contentInset.top = scrollView.contentInset.top + s.height
-        }
+        }) 
         
-        fireEvent(.StartRefreshing)
+        fireEvent(.startRefreshing)
     }
     
-    private func endRefresh() {
+    fileprivate func endRefresh() {
         guard let scrollView = superview as? UIScrollView else { return }
-        if state == .None { return }
-        fireEvent(.EndRefreshing)
+        if state == .none { return }
+        fireEvent(.endRefreshing)
         scrollView.contentInset.top = scrollView.contentInset.top - height
         scrollView.contentOffset.y = scrollView.contentOffset.y - height
         let initialPoint = CGPoint(x: scrollView.contentOffset.x, y: scrollView.contentOffset.y + height)
         scrollView.setContentOffset(initialPoint, animated: true)
 
         let delay = 0.25 * Double(NSEC_PER_SEC)
-        let when  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-        dispatch_after(when, dispatch_get_main_queue()) { [weak self] () -> Void in
+        let when  = DispatchTime.now() + Double(Int64(delay)) / Double(NSEC_PER_SEC)
+        DispatchQueue.main.asyncAfter(deadline: when) { [weak self] () -> Void in
             guard let s = self else { return }
-            s.fireEvent(.RecoveredToInitialState)
+            s.fireEvent(.recoveredToInitialState)
         }
     }
     
-    private func fireEvent(event: SwiftRefresherEvent) {
+    fileprivate func fireEvent(_ event: SwiftRefresherEvent) {
         switch event {
-        case .Pull:
-            stateInternal = .Pulling
-        case .StartRefreshing:
-            stateInternal = .Refreshing
+        case .pull:
+            stateInternal = .pulling
+        case .startRefreshing:
+            stateInternal = .refreshing
             startRefreshingHandler?()
-        case .EndRefreshing:
-            stateInternal = .RecoveringInitialState
-        case .RecoveredToInitialState:
-            stateInternal = .None
+        case .endRefreshing:
+            stateInternal = .recoveringInitialState
+        case .recoveredToInitialState:
+            stateInternal = .none
         }
-        eventHandler?(event: event)
+        eventHandler?(event)
         refreshView.didReceiveEvent(event)
     }
 }
